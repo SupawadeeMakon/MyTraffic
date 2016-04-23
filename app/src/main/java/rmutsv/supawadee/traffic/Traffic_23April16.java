963d21dd6758fbcff723e9e7a3ea6c379c0e7517
SupawadeeMakon <supawadee018@gmail.com>
2016-04-23 11:32:52 +0700

    Start App

---
+++ app/src/main/java/rmutsv/supawadee/traffic/Traffic_23April16.java
@@ -0,0 +1,13 @@
+package rmutsv.supawadee.traffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Traffic_23April16 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traffic_23_april16);
+    }
+}
